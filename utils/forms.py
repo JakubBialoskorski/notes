53a9ccffde57d27922c0dedcf80f37d4d4a272a5
Jakub Białoskórski <bialoskorski@icloud.com>
2020-10-12 22:50:51 +0200

    set min / max password required length

--- utils/forms.py
+++ utils/forms.py
@@ -11,7 +11,7 @@ class LoginForm(FlaskForm):
 class SignUpForm(FlaskForm):
     username = TextField('Username*', [validators.Required("Please enter your username")])
     email = TextField('Email*', [validators.Required("Please enter your email"), validators.Email('Email format incorrect')])
-    password = PasswordField('Password*', [validators.Required("Please enter your password"), validators.EqualTo('confirm_password', message='Passwords must match')])
+    password = PasswordField('Password*', [validators.Required("Please enter your password"), validators.EqualTo('confirm_password', message='Passwords must match'), validators.Length(min=8, max=32, message='Password must contain 8 digits minimum, with 32 being maximum')])
     confirm_password = PasswordField('Confirm Password*', [validators.Required("Confirm your password")])
     submit = SubmitField('Signup')
 
@@ -31,6 +31,6 @@ class ChangeEmailForm(FlaskForm):
     submit = SubmitField('Update Email')
 
 class ChangePasswordForm(FlaskForm):
-    password = PasswordField('Password*', [validators.Required("Please enter your password"), validators.EqualTo('confirm_password', message='Passwords must match')])
+    password = PasswordField('Password*', [validators.Required("Please enter your password"), validators.EqualTo('confirm_password', message='Passwords must match'), validators.Length(min=8, max=32, message='Password must contain 8 digits minimum, with 32 being maximum')])
     confirm_password = PasswordField('Confirm Password*', [validators.Required("Confirm your password")])
     submit = SubmitField('Update Password')
