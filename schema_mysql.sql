99c858437c02679b64995f9bf2f08552fab6e758
Jakub Białoskórski <bialoskorski@icloud.com>
2021-07-18 16:58:07 +0200

    rework MySQL schema in proper order

--- schema_mysql.sql
+++ schema_mysql.sql
@@ -1,3 +1,17 @@
+CREATE TABLE users (
+	id INT NOT NULL AUTO_INCREMENT,
+	username TEXT(255),
+	password TEXT(200),
+	email TEXT(200),
+	PRIMARY KEY (id)
+);
+CREATE TABLE tags (
+	id INT NOT NULL AUTO_INCREMENT,
+	tag TEXT,
+	user_id INT,
+	PRIMARY KEY (id),
+	FOREIGN KEY(user_id) REFERENCES users(id)
+);
 CREATE TABLE notes (
 	id INT NOT NULL AUTO_INCREMENT,
 	note_title TEXT(255),
@@ -7,18 +21,4 @@ CREATE TABLE notes (
 	user_id INT,
 	PRIMARY KEY (id),
 	FOREIGN KEY(user_id) REFERENCES users(id)
-);
-CREATE TABLE tags (
-	id INT NOT NULL AUTO_INCREMENT,
-	tag TEXT,
-	user_id INT,
-	PRIMARY KEY (id),
-	FOREIGN KEY(user_id) REFERENCES users(id)
-);
-CREATE TABLE users (
-	id INT NOT NULL AUTO_INCREMENT,
-	username TEXT(255),
-	password TEXT(200),
-	email TEXT(200),
-	PRIMARY KEY (id)
 );
